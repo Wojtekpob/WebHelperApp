87b4e8455033ec14c2c71569ab7912c374ce2f4a
Wojciech Pobocha <01168909@pw.edu.pl>
2023-02-05 11:17:39 +0100

    Created project with Spring Web, Spring Data JPA, MySQL Drvier and Lombok dependencies.

---
+++ src/main/java/com/study/StudyHelperApp/StudyHelperAppApplication.java
@@ -0,0 +1,13 @@
+package com.study.StudyHelperApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHelperAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/StudyHelperApp/StudyHelperAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.StudyHelperApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHelperAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
