afdb45beab082a1c4af19fa254be12f7b85ed05b
Wojciech Pobocha <01168909@pw.edu.pl>
2023-02-06 10:57:35 +0100

    Started implementing jwt token authentication.

--- src/main/java/com/study/StudyHelperApp/config/JwtAuthenticationFilter.java
+++ src/main/java/com/study/StudyHelperApp/config/JwtAuthenticationFilter.java
@@ -4,16 +4,36 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
+        String authorizatorinHeader = request.getHeader("Authorization");
+        String jwt;
+        String username;
+        if (authorizatorinHeader==null || !authorizatorinHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        jwt = authorizatorinHeader.substring(7);
+        username = jwtService.getUsernameFromToken(jwt);
+        //TODO implement further authentication by getting userDetails and validating him
     }
 }
+
+
---
+++ src/main/java/com/study/StudyHelperApp/config/JwtService.java
@@ -0,0 +1,41 @@
+package com.study.StudyHelperApp.config;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+@NoArgsConstructor
+public class JwtService {
+    private static final String SECRET_KEY = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+
+    public String getUsernameFromToken(String jwtToken){
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    private <T> T extractClaim(String jwtToken, Function<Claims,T> claimsTFunction) {
+        Claims claims = extractAllClaims(jwtToken);
+        return claimsTFunction.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
