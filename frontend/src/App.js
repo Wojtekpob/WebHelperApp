8cbb098d3fa3e8667e19676c07670bd547f4ab2d
Wojciech Pobocha <01168909@pw.edu.pl>
2023-02-14 18:04:17 +0100

    Implemented page and logic behind the registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./homepage";
+import Login from "./login";
 import Register from "./register";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Homepage />} />
       <Route path="register" element={<Register />} />
+      <Route path="login" element={<Login />} />
     </Routes>
   );
 }
--- frontend/src/register/index.js
+++ frontend/src/register/index.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocalState } from "../service/useLocalStorage";
 
 const Register = () => {
-  return <div>Registration</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  function sendRegistrationRequest() {
+    const requestBody = {
+      name: name,
+      surname: surname,
+      username: username,
+      password: password,
+    };
+    fetch("rest/auth/register", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        else return Promise.reject("Invalid Data");
+      })
+      .then(() => {
+        window.location.href = "login";
+      })
+      .catch((message) => {
+        window.alert(message);
+      });
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="surname">Surname</label>
+        <input
+          id="surname"
+          value={surname}
+          onChange={(event) => setSurname(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+      <div>
+        <button
+          id="submit"
+          type="button"
+          onClick={() => sendRegistrationRequest()}
+        >
+          Register
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Register;
