3491aea830c540f0183ea06d1ca231052655fc7a
Wojciech Pobocha <01168909@pw.edu.pl>
2023-02-14 16:47:02 +0100

    Implemented helping function useLocalStorage to store jwt token.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./homepage";
+import Register from "./register";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="register" element={<Register />} />
     </Routes>
   );
 }
