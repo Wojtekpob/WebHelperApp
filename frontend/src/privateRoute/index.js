e55cf71d9172b692918b6e6cbc8bc1fd96a4ec06
Wojciech Pobocha <01168909@pw.edu.pl>
2023-02-18 09:46:20 +0100

    Implemented private route request for jwt validation

--- frontend/src/privateRoute/index.js
+++ frontend/src/privateRoute/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocalState } from "../util/useLocalStorage";
 import { Navigate } from "react-router-dom";
+import sendRequest from "../service/sendRequest";
 
 const PrivateRoute = ({ children }) => {
   const [jwt, setJwt] = useLocalState("", "jwt");
-  return jwt ? children : <Navigate to="/login" />;
+  const [loading, setLoading] = useState(true);
+  const [isValid, setIsValid] = useState(null);
+
+  if (jwt) {
+    sendRequest(`/validate?token=${jwt}`, "GET", jwt).then((isValid) => {
+      setIsValid(isValid);
+      setLoading(false);
+    });
+  } else {
+    return <Navigate to="/login" />;
+  }
+  return loading ? (
+    <div>Loading</div>
+  ) : isValid ? (
+    children
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default PrivateRoute;
