e3f9c361907a197500f00114325c9c2668b61b41
Wojciech Pobocha <01168909@pw.edu.pl>
2023-02-19 13:08:50 +0100

    Added Assignment class.

---
+++ src/main/java/com/study/StudyHelperApp/assignment/Assignment.java
@@ -0,0 +1,22 @@
+package com.study.StudyHelperApp.assignment;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Assignment {
+    @Id @GeneratedValue
+    private Long id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String comments;
+    //TODO list of images
+}
