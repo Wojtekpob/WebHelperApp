ae8387f368cb14bf91342b04cc76c733bd8d849b
Wojciech Pobocha <01168909@pw.edu.pl>
2023-03-24 00:57:08 +0100

    Updated register view

--- frontend/src/register/index.js
+++ frontend/src/register/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
 import sendRequest from "../service/sendRequest";
 import { useLocalState } from "../service/useLocalStorage";
 
@@ -9,6 +10,18 @@ const Register = () => {
   const [surname, setSurname] = useState("");
   const [jwt, setJwt] = useLocalState("", "jwt");
 
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
   function sendRegistrationRequest() {
     const requestBody = {
       name: name,
@@ -16,6 +29,7 @@ const Register = () => {
       username: username,
       password: password,
     };
+    console.log(requestBody);
     sendRequest("rest/auth/register", "POST", jwt, requestBody)
       .then((response) => {
         console.log(response);
@@ -28,48 +42,79 @@ const Register = () => {
 
   return (
     <>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="surname">Surname</label>
-        <input
-          id="surname"
-          value={surname}
-          onChange={(event) => setSurname(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </div>
-      <div>
-        <button
-          id="submit"
-          type="button"
-          onClick={() => sendRegistrationRequest()}
-        >
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="4" controlId="validationCustom01">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+          </Form.Group>
+          <Row>
+            <Form.Group as={Col} md="4" controlId="validationCustom02">
+              <Form.Label>Surname</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                placeholder="Enter surname"
+                value={surname}
+                onChange={(event) => setSurname(event.target.value)}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            </Form.Group>
+          </Row>
+          <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+            <Form.Label>Username</Form.Label>
+            <InputGroup hasValidation>
+              <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                aria-describedby="inputGroupPrepend"
+                required
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+              />
+              <Form.Control.Feedback type="invalid">
+                Please choose a username.
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
+        </Row>
+        <Form.Group as={Col} md="4" controlId="validationCustomPassword">
+          <Form.Label>Password</Form.Label>
+          <InputGroup hasValidation>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              aria-describedby="inputGroupPrepend"
+              required
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please choose a password.
+            </Form.Control.Feedback>
+          </InputGroup>
+        </Form.Group>
+        <Row className="mb-3"></Row>
+        <Form.Group className="mb-3">
+          <Form.Check
+            required
+            label="Agree to terms and conditions"
+            feedback="You must agree before submitting."
+            feedbackType="invalid"
+          />
+        </Form.Group>
+        <Button type="submit" onClick={() => sendRegistrationRequest()}>
           Register
-        </button>
-      </div>
+        </Button>
+      </Form>
     </>
   );
 };
